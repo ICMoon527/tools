85c100e19df08ff5733ac1698a1e1c8b7f45d235
ICMoon527 <11830026@zju.edu.cn>
2022-05-21 23:53:46 +0800

    cvtPic2Base

--- cvtPic2Base.py
+++ cvtPic2Base.py
@@ -1,8 +1,12 @@
 import base64
-f=open('D:\Study\PHD4\ASD\ASD\Doc\shared space结构.png','rb') #二进制方式打开图文件
+f=open('D:\Study\PHD4\ASD\ASD\Doc\Learning Visual Attention.png','rb') #二进制方式打开图文件
 ls_f=base64.b64encode(f.read()) #读取文件内容，转换为base64编码
 f.close()
-print(ls_f)
+# print(ls_f)
+f = open('Base.txt', 'w')
+ls = ls_f.decode()
+f.write(ls)
+f.close()
 
 # # 将base转化为图片
 # import base64
@@ -11,3 +15,6 @@ print(ls_f)
 # file=open('2.jpg','wb')
 # file.write(imgdata)
 # file.close()
+
+# ![avatar][base64str]
+# [base64str]:data:image/png;base64,iVBO...
